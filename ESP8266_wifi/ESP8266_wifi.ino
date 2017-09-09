d18d30dfdd94cba0b538dc090d99e191790523f8
ab790815 <ab790815@gmail.com>
2017-09-09 09:53:59 +0800

    .

--- ESP8266_wifi/ESP8266_wifi.ino
+++ ESP8266_wifi/ESP8266_wifi.ino
@@ -13,29 +13,38 @@ ESP8266WebServer server_AP(80);
 char *ssid_Clinet     = "";
 char *password_Clinet = "";
 
+int LED = 16;
+
 const char INDEX_HTML[] =
   "<!DOCTYPE html>"
   "<html>"
-  " <head>"
-  " <meta http-equiv=\"Content-Type\" content=\"text/html; charset=Big5\">"
-  "   <title>ESP8266 Web Form Demo</title>"
-  "   <style>"
-  "     \"body { background-color: #808080; font-family: Arial, Helvetica, Sans-Serif; Color: #000000; }\""
-  "   </style>"
-  " </head>"
-  " <body>"
-  "   <FORM action=\"/\" method=\"post\">"
-  "     <h1>輸入Wifi名稱及密碼</h1>"
-  "     SSID <input type=\"text\" name=\"ssid\"/><BR>"
-  "     密碼 <input type=\"text\" name=\"passwd\"/><BR>"
-  "     <input type=\"submit\" value=\"送出\" />"
-  "   </FORM>"
-  " </body>"
+  "    <head>"
+  "        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=Big5\">"
+  "        <title>ESP8266 Web Form Demo</title>"
+  "        <style>\"body { background-color: #808080; font-family: Arial, Helvetica, Sans-Serif; Color: #000000; }\"</style>"
+  "    </head>"
+  "    <body>"
+  "        <FORM action=\"/\" method=\"post\">"
+  "            <h1>輸入Wifi名稱及密碼</h1>"
+  "            SSID <input type=\"text\" name=\"ssid\"/><BR>"
+  "            密碼 <input type=\"text\" name=\"passwd\"/><BR>"
+  "            <input type=\"submit\" value=\"送出\" />"
+  "        </FORM>"
+  "        <FORM action=\"/\" method=\"post\">"
+  "            <h1>Set LED</h1>"
+  "            <INPUT type=\"radio\" name=\"LED\" value=\"1\" />On<BR>"
+  "            <INPUT type=\"radio\" name=\"LED\" value=\"0\" />Off<BR>"
+  "            <INPUT type=\"submit\" value=\"Set\" />"
+  "        </FORM>"
+  "    </body>"
   "</html>";
 
 void handleRoot() {
   if (server_AP.hasArg("ssid")) {
     handleSubmit();
+  }
+  else if (server_AP.hasArg("LED")) {
+    handleSubmit_LED();
   }
   else {
     server_AP.send(200, "text/html", INDEX_HTML);
@@ -53,9 +62,9 @@ void handleSubmit()
 {
   if (!server_AP.hasArg("ssid")) return returnFail("BAD ARGS");
   if (!server_AP.hasArg("passwd")) return returnFail("BAD ARGS");
-  
+
   String data1 = server_AP.arg("ssid");
-  ssid_Clinet = const_cast<char*>(data1.c_str());  
+  ssid_Clinet = const_cast<char*>(data1.c_str());
   String data2 = server_AP.arg("passwd");
   password_Clinet = const_cast<char*>(data2.c_str());
   Serial.println("----------------------");
@@ -66,7 +75,7 @@ void handleSubmit()
   Serial.println("----------------------");
 
   Serial.println("OK");
-//  WiFi.begin(string2char(server_AP.arg("ssid")), string2char(server_AP.arg("passwd")));   //
+  //  WiFi.begin(string2char(server_AP.arg("ssid")), string2char(server_AP.arg("passwd")));   //
   WiFi.begin(ssid_Clinet, password_Clinet);   //
   Serial.println("");
   int retry = 0;
@@ -79,9 +88,28 @@ void handleSubmit()
 
   server_AP.send(200, "text/html", INDEX_HTML);
 }
+
+void handleSubmit_LED()
+{  
+  if (!server_AP.hasArg("LED")) return returnFail("BAD ARGS");
+  String LEDvalue = server_AP.arg("LED");
+  if (LEDvalue == "1") {
+    digitalWrite(LED, HIGH);
+    server_AP.send(200, "text/html", INDEX_HTML);
+  }
+  else if (LEDvalue == "0") {
+    digitalWrite(LED, LOW);
+    server_AP.send(200, "text/html", INDEX_HTML);
+  }
+  else {
+    returnFail("Bad LED value");
+  }
+}
+
 void setup() {
   delay(1000);
   Serial.begin(115200);
+  pinMode(LED, OUTPUT);
   Serial.println();
   Serial.print("Configuring access point...");
   /* You can remove the password parameter if you want the AP to be open. */
